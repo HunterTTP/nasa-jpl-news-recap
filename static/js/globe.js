ba6de03160c6746425ad33ecd7cddc636ed85d95
HunterTTP <HunterTTP@gmail.com>
2023-04-09 14:07:33 -0400

    Added rover positions to Mars

--- static/js/globe.js
+++ static/js/globe.js
@@ -5,3 +5,21 @@ function createGlobe() {
     globe = new THREE.Mesh(globeGeometry, globeMaterial);
     scene.add(globe);
 }
+
+function createRoverMarker(latitude, longitude, roverName) {
+    const phi = (90 - latitude) * (Math.PI / 180);
+    const theta = (longitude + 180) * (Math.PI / 180);
+
+    const roverMarkerGeometry = new THREE.SphereGeometry(0.01, 16, 16);
+    const roverMarkerMaterial = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
+    const roverMarker = new THREE.Mesh(roverMarkerGeometry, roverMarkerMaterial);
+
+    roverMarker.position.x = -Math.sin(phi) * Math.cos(theta);
+    roverMarker.position.y = Math.cos(phi);
+    roverMarker.position.z = Math.sin(phi) * Math.sin(theta);
+    roverMarker.position.multiplyScalar(0.991);
+
+    roverMarker.name = roverName;
+
+    return roverMarker;
+}
--- static/js/init.js
+++ static/js/init.js
@@ -1,4 +1,4 @@
-let container, camera, scene, renderer, globe, satellites, controls;
+let container, camera, scene, renderer, globe, satellites, controls, labelRenderer;
 
 function initThreeJs(satelliteData) {
     container = document.getElementById('globe-container');
@@ -16,9 +16,23 @@ function initThreeJs(satelliteData) {
     renderer.setSize(container.clientWidth, container.clientHeight);
     container.appendChild(renderer.domElement);
 
-
     // Add the globe to the scene
     createGlobe();
+
+    // Add rover markers to the scene using hardcoded coordinates
+    const roverLocations = [
+        { name: "Sojourner", latitude: 19.13, longitude: -33.22 },
+        { name: "Spirit", latitude: -14.57, longitude: 175.47 },
+        { name: "Opportunity", latitude: -1.95, longitude: -354.47 },
+        { name: "Curiosity", latitude: -4.59, longitude: 137.44 },
+        { name: "Perseverance", latitude: 18.4446, longitude: 77.4509 }
+    ];
+
+    roverLocations.forEach(rover => {
+        const roverMarker = createRoverMarker(parseFloat(rover.latitude), parseFloat(rover.longitude));
+        scene.add(roverMarker);
+    });
+
 
     // Add a light source
     const light = new THREE.PointLight(0xffffff, 1, 100);
