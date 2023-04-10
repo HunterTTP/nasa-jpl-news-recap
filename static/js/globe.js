106947ba769be92a402d8ef72522a5ce067055db
Hunter Ellis <hunter@Hunters-Air.attlocal.net>
2023-04-09 22:19:42 -0400

    Updated background, css and fonts

--- static/js/globe.js
+++ static/js/globe.js
@@ -10,14 +10,14 @@ function createRoverMarker(latitude, longitude, roverName) {
     const phi = (90 - latitude) * (Math.PI / 180);
     const theta = (longitude + 180) * (Math.PI / 180);
 
-    const roverMarkerGeometry = new THREE.SphereGeometry(0.01, 16, 16);
-    const roverMarkerMaterial = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
+    const roverMarkerGeometry = new THREE.SphereGeometry(0.015, 16, 16);
+    const roverMarkerMaterial = new THREE.MeshBasicMaterial({ color: 0xFF8C00 });
     const roverMarker = new THREE.Mesh(roverMarkerGeometry, roverMarkerMaterial);
 
     roverMarker.position.x = -Math.sin(phi) * Math.cos(theta);
     roverMarker.position.y = Math.cos(phi);
     roverMarker.position.z = Math.sin(phi) * Math.sin(theta);
-    roverMarker.position.multiplyScalar(0.991);
+    roverMarker.position.multiplyScalar(0.99);
 
     roverMarker.name = roverName;
 
