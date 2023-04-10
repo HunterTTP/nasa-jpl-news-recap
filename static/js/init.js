d0adb66c05e972ce2f625527dbc6ba1e2e3ad6d2
HunterTTP <HunterTTP@gmail.com>
2023-04-10 15:42:57 -0400

    Updated init.js to remove unecessary comments and make it more readable

--- static/js/init.js
+++ static/js/init.js
@@ -1,24 +1,12 @@
 let container, camera, scene, renderer, globe, satellites, labelRenderer;
 
-function initThreeJs(satelliteData) {
+function initThreeJs() {
     container = document.getElementById('globe-container');
-
-    // Set up the scene, camera, renderer, and controls
     setupScene();
-
-    // Add the globe to the scene
     addGlobeToScene();
-
-    // Add rover markers to the scene
     addRoverMarkersToScene();
-
-    // Set up the controls
     setupControls();
-
-    // Add a light source
     addLightSourceToScene();
-
-    // Render loop
     animateScene();
 }
 
