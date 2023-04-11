466e7d4a3d64a48a20944f45f673f2854bb9e51b
HunterTTP <HunterTTP@gmail.com>
2023-04-10 21:25:49 -0400

    Updated initial globe size based on viewport size

--- static/js/controls.js
+++ static/js/controls.js
@@ -8,9 +8,14 @@ function setupControls() {
     controls.dampingFactor = 0.05;
     controls.screenSpacePanning = false;
     controls.minDistance = 1.3;
-    controls.maxDistance = 10;
+    controls.maxDistance = 5;
 
-    // Set initial camera position
-    camera.position.set(3, 0, 0);
+    // Set initial camera position based on screen width
+    if (window.innerWidth < 768) {
+        camera.position.set(3, 0, 0);
+    } else {
+        camera.position.set(2.5, 0, 0);
+    }
+
     controls.update();
 }
